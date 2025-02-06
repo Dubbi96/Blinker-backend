468415256cb643b1dbc2e79ee73af7c543182879
Dubbi96 <yrs03001@hanyang.ac.kr>
2025-02-06 20:11:47 +0900

    chore : pushing for rebuild

--- startup.sh
+++ startup.sh
@@ -3,6 +3,8 @@
 echo "Cloud Storage에서 SSL 인증서 다운로드"
 
 # Cloud Storage에서 client-keystore.p12 다운로드
+# 디렉토리 생성
+mkdir -p /app/config
 gsutil cp gs://blinker-backend-ssl/client-keystore.p12 /app/config/client-keystore.p12
 
 # 파일 권한 변경 (읽기 가능하도록)
