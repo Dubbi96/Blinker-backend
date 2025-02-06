1b65ff09152cf29d9b82f17cdc35f84f2dfa2c51
Dubbi96 <yrs03001@hanyang.ac.kr>
2025-02-06 18:52:44 +0900

    fix : slack logger 설정 수정

--- src/main/java/com/blinker/atom/domain/GroupRepository.java
+++ src/main/java/com/blinker/atom/domain/GroupRepository.java
@@ -1,6 +1,8 @@
 package com.blinker.atom.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GroupRepository extends JpaRepository<SensorGroup, Long> {
 }
--- src/main/java/com/blinker/atom/domain/UserGroupRepository.java
+++ src/main/java/com/blinker/atom/domain/UserGroupRepository.java
@@ -1,6 +1,8 @@
 package com.blinker.atom.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserGroupRepository extends JpaRepository<UserGroup, Long> {
 }
