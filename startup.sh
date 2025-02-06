019e44a24a5ad8a8a8e9fe3518b81a931d5aae5a
cm-JongwonKang <jongwon_kang@tmax.co.kr>
2025-02-06 20:18:28 +0900

    chore : pushing for rebuild

--- startup.sh
+++ startup.sh
@@ -2,14 +2,27 @@
 
 echo "Cloud Storage에서 SSL 인증서 다운로드"
 
-# Cloud Storage에서 client-keystore.p12 다운로드
-# 디렉토리 생성
+# gsutil이 없을 경우 설치
+if ! command -v gsutil &> /dev/null
+then
+    echo "gsutil이 설치되지 않음. 설치 진행..."
+    apt-get update && apt-get install google-cloud-sdk -y
+fi
+
+# /app/config 경로 생성
 mkdir -p /app/config
-gcloud storage cp gs://blinker-backend-ssl/client-keystore.p12 /app/config/client-keystore.p12
-# 파일 권한 변경 (읽기 가능하도록)
+
+# Cloud Storage에서 client-keystore.p12 다운로드
+gsutil cp gs://blinker-backend-ssl/client-keystore.p12 /app/config/client-keystore.p12
+
+# 파일 권한 변경
 chmod 600 /app/config/client-keystore.p12
 
 echo "SSL 인증서 다운로드 완료!"
+
+# SSL 인증서 검증
+echo "SSL 인증서 확인..."
+keytool -list -keystore /app/config/client-keystore.p12 -storepass certificate-keystore-password-123
 
 # 애플리케이션 실행
 echo "애플리케이션 시작..."
