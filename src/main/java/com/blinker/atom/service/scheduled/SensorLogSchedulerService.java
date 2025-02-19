0c49398754cd21b3c8a0a0ea07a5da55823cea41
Dubbi96 <yrs03001@hanyang.ac.kr>
2025-02-20 05:35:47 +0900

    fix : thread 문제 해결

--- src/main/java/com/blinker/atom/service/scheduled/SensorLogSchedulerService.java
+++ src/main/java/com/blinker/atom/service/scheduled/SensorLogSchedulerService.java
@@ -71,7 +71,7 @@ public class SensorLogSchedulerService {
      *  5-1-9. cmd 71번의 경우도 GPS 좌표이나, 67번, 73로그 둘다 가지고 있으므로 로그 무시
      * 	6.	해당 작업을 Spring Scheduler + Async를 이용해 주기적으로 실행.
      * 	*/
-    @Scheduled(fixedRate = 86400000)  // 1일 1회 실행 (1000ms * 60 * 60 * 24)
+    /*@Scheduled(fixedRate = 86400000)  // 1일 1회 실행 (1000ms * 60 * 60 * 24)
     public void fetchAndSaveSensorLogs() {
         try {
             log.info("🔹 Sensor Log 스케줄러 실행 중...");
@@ -79,11 +79,12 @@ public class SensorLogSchedulerService {
         } catch (Exception e) {
             log.error("스케줄러 실행 중 예외 발생", e);
         }
-    }
+    }*/
 
-    @Async
+    @Scheduled(fixedRate = 86400000)  // 1일 1회 실행 (1000ms * 60 * 60 * 24)
     @Transactional
-    public void asyncFetchAndSaveSensorLogs() {
+    public void fetchAndSaveSensorLogs() {
+        log.info("🔹 Sensor Log 스케줄러 실행 중...");
         // 모든 sensor_group 조회
         List<SensorGroup> sensorGroups = sensorGroupRepository.findAll();
 
